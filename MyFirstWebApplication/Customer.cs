75cc154f0b4ca564c01b7ecffadfaa72ea489957
nspasov <nspasov22@gmail.com>
2018-10-23 20:11:53 +0300

    application working

---
+++ MyFirstWebApplication/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstWebApplication
+{
+    public class Customer
+    {
+        [Required, StringLength(10)]
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+    }
+}
